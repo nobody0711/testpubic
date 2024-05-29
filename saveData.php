f69aae3cd732c019589cbffde0fcb718f7251237
nobody0711 <m7mmed02.n.a@gmail.com>
2024-05-29 09:35:12 -0400

    test

---
+++ saveData.php
@@ -0,0 +1,24 @@
+<?php
+
+// Retrieve posted data
+$postData = $_POST['postData'];
+
+// Separate the data by newline character (\n)
+$dataArray = explode("\n", $postData);
+
+// Output formatted data
+foreach ($dataArray as $data) {
+    echo $data . "<br>";
+}
+
+// Write data to file
+$file = '/var/www/html/data.txt';
+$current = file_get_contents($file);
+$current .= $postData . "\n";
+file_put_contents($file, $current);
+
+// Two empty fields for the next post message
+echo "Empty Field 1: <br>";
+echo "Empty Field 2: <br>";
+?>
+
